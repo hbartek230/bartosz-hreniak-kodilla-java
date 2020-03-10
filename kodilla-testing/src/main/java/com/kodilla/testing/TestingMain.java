071e9c1c51eab6a6ed1ad6b3b9e9f1913f7eb85f
hbartek230 <bhreniak@gmail.com>
2020-03-10 17:48:46 +0100

    This is module 6.2 in a new branch and PR

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("\nAnother test: ");
+
+        Calculator calculator = new Calculator();
+
+        System.out.println("This is first calculator method: " + calculator.add(2, 4));
+        System.out.println("This is second calculator method: " + calculator.substract(3, 1));
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        int sum;
+        sum = a + b;
+        return sum;
+    }
+
+    public int substract(int a, int b){
+        int sub;
+        sub = a - b;
+        return sub;
+    }
+
+}
