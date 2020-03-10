09f2a0a40617315717cbf808063f375c76de8e63
hbartek230 <bhreniak@gmail.com>
2020-03-10 22:53:24 +0100

    Module 6.3 changes

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,26 +1,18 @@
 package com.kodilla.testing.collection;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class OddNumbersExterminator {
-    public void exterminate(ArrayList<Integer> numbers, int count) {
-        if(count != 0) {
-            for (int i = 0; i < count; i++) {
-                numbers.add(new Random().nextInt(101));
-            }
-        } else {
-            System.out.println("Count can`t be negative or equals 0");
-        }
-        ArrayList<Integer> oddNumbers = new ArrayList<Integer>();
+    ArrayList<Integer> oddNumbers;
 
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) % 2 == 0) {
-                oddNumbers.add(numbers.get(i));
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+        if (numbers.size() != 0) {
+            oddNumbers = new ArrayList<Integer>();
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i) % 2 == 0) {
+                    oddNumbers.add(numbers.get(i));
+                }
             }
-        }
-        for(int x : oddNumbers){
-            System.out.println("Odd numbers: " + x);
-        }
+        } return oddNumbers;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -4,30 +4,35 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class CollectionTestSuite {
 
     @Before
-    public void befoer(){
+    public void befoer() {
         System.out.println("Test case: begin");
     }
 
     @After
-    public void after(){
+    public void after() {
         System.out.println("Test case: end");
     }
 
     @Test
-    public void testOddNumbersExterminatorEmptyList(){
+    public void testOddNumbersExterminatorEmptyList() {
         OddNumbersExterminator one = new OddNumbersExterminator();
-        one.exterminate(new ArrayList<Integer>(), 0);
+        one.exterminate(new ArrayList<Integer>());
+        if (one.oddNumbers == null) {
+            System.out.println("Pusta lista");
+        }
     }
 
     @Test
-    public void testOddNumbersExterminatorNormalList(){
+    public void testOddNumbersExterminatorNormalList() {
         OddNumbersExterminator one = new OddNumbersExterminator();
-        one.exterminate(new ArrayList<Integer>(), 20);
+        one.exterminate(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+                18, 19, 20)));
+        System.out.println("Wypelniona lista: " + one.oddNumbers);
     }
 
 }
