1c5195368c8370fe84acb3d5e4542b4a5d55bf0d
hbartek230 <bhreniak@gmail.com>
2020-03-20 21:00:48 +0100

    Prepare to submodule 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,36 +1,19 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
-        Processor processor = new Processor();
-        Executor codeToExecute = () -> System.out.println("This is an example text.");
-        processor.execute(codeToExecute);
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
 
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        String textToTest = "Bartek";
-        PoemBeautifier pb = new PoemBeautifier();
-        pb.beautify(textToTest, (decoration -> "Jestem " + textToTest));
-        pb.beautify(textToTest, (decoration -> textToTest + " Hreniak"));
-        pb.beautify(textToTest, (decoration -> textToTest.toUpperCase()));
-        pb.beautify(textToTest, (decoration -> textToTest.toUpperCase().toLowerCase()));
-        pb.beautify(textToTest, (decoration -> "---" + textToTest));
-        pb.beautify(textToTest, (decoration -> textToTest + "---"));
-        pb.beautify(textToTest, (decoration -> "---" + textToTest + "---"));
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        System.out.println(theResultStringOfBooks);
     }
 }
