2e9e8b5f6dbcabd56555e8aaf53369a5c571c89c
hbartek230 <bhreniak@gmail.com>
2020-04-30 13:55:35 +0200

    Rest of module 13.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -8,38 +8,47 @@ import java.util.stream.Collectors;
 
 public class MovieStore {
 
+    private static final String IRONMAN_PL = "Żelazny człowiek";
+    private static final String IRONMAN_ENG = "Iron Man";
+    private static final String AVENGERS_PL = "Mściciele";
+    private static final String AVENGERS_ENG = "Avengers";
+    private static final String FLASH_PL = "Błyskawica";
+    private static final String FLASH_ENG = "Flash";
+    private static final String IRONMAN_SHORT = "IM";
+    private static final String AVENGERS_SHORT = "AV";
+    private static final String FLASH_SHORT = "FL";
+
     public Map<String, List<String>> getMovies() {
 
         List<String> ironManTranslations = new ArrayList<>();
-        ironManTranslations.add("Żelazny Człowiek");
-        ironManTranslations.add("Iron Man");
+        ironManTranslations.add(IRONMAN_PL);
+        ironManTranslations.add(IRONMAN_ENG);
 
         List<String> avengersTranslations = new ArrayList<>();
-        avengersTranslations.add("Mściciele");
-        avengersTranslations.add("Avengers");
+        avengersTranslations.add(AVENGERS_PL);
+        avengersTranslations.add(AVENGERS_ENG);
 
         List<String> flashTranslations = new ArrayList<>();
-        flashTranslations.add("Błyskawica");
-        flashTranslations.add("Flash");
+        flashTranslations.add(FLASH_PL);
+        flashTranslations.add(FLASH_ENG);
 
         Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
-        booksTitlesWithTranslations.put("IM", ironManTranslations);
-        booksTitlesWithTranslations.put("AV", avengersTranslations);
-        booksTitlesWithTranslations.put("FL", flashTranslations);
+        booksTitlesWithTranslations.put(IRONMAN_SHORT, ironManTranslations);
+        booksTitlesWithTranslations.put(AVENGERS_SHORT, avengersTranslations);
+        booksTitlesWithTranslations.put(FLASH_SHORT, flashTranslations);
 
         return booksTitlesWithTranslations;
     }
 
     public static void main(String[] args) {
         MovieStore movies = new MovieStore();
-        Map<String, List<String>> testedList = movies.getMovies();
 
-
-        String kartofel = testedList.values()
+        String tested = movies.getMovies()
+                .values()
                 .stream()
                 .map(movie -> movie.toString())
                 .collect(Collectors.joining("!"));
-        System.out.println(kartofel);
+        System.out.println(tested);
     }
 
 }
