75880e2b05d3b7e1ff69f391706d7d14f5fdfd7e
hbartek230 <bhreniak@gmail.com>
2020-05-18 18:32:40 +0200

    Module 15.5 code review changes

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigMacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigMacTestSuite.java
@@ -3,6 +3,8 @@ package com.kodilla.patterns.builder.bigmac;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class BigMacTestSuite {
 
     @Test
@@ -11,15 +13,22 @@ public class BigMacTestSuite {
         Bigmac bigmac = new Bigmac.BigmacBuilder()
                 .bun("Sezam")
                 .burgers(2)
+                .ingredients("salad")
                 .sauce("barbecue")
+                .ingredients("onion")
+                .ingredients("becon")
                 .build();
 
         // when
         int howManyBurgers = bigmac.getBurgers();
         String whichBun = bigmac.getBun();
+        int howManyIngredients = bigmac.getIngredients().size();
+        String whichSauce = bigmac.getSauce();
 
         // then
         Assert.assertEquals(2, howManyBurgers);
         Assert.assertEquals("Sezam", whichBun);
+        Assert.assertEquals(3, howManyIngredients);
+        Assert.assertEquals("barbecue", whichSauce);
     }
 }
