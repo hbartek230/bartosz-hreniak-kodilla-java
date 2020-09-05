bb07d1abf0d75b30aa0241873c4fc22498388ebb
hbartek230 <bhreniak@gmail.com>
2020-09-05 13:25:38 +0200

    Prepare to module 31.1

--- kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -1,7 +1,8 @@
 package com.kodilla.stream.invoice.simple;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SimpleInvoiceTestSuite {
     @Test
@@ -13,6 +14,6 @@ public class SimpleInvoiceTestSuite {
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5));
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 3",  6.49), 5.0));
         //Then
-        Assert.assertEquals(108.975, invoice.getValueToPay(), 0.001);
+        assertEquals(108.975, invoice.getValueToPay(), 0.001);
     }
 }
